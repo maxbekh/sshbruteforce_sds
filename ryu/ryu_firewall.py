367484c3813c72e4bff2cf906f16050899a7e7b2
BEKHEDDA Maxence <maxence.bekhedda@telecomnancy.eu>
2024-05-13 20:25:23 +0200

    add nmap to attacker and log to firewall

--- ryu/ryu_firewall.py
+++ ryu/ryu_firewall.py
@@ -4,6 +4,7 @@ from ryu.controller import handler
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.ofproto import ofproto_v1_3
 from datetime import datetime, timedelta
+import logging
 
 class SSHFirewall(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -12,6 +13,12 @@ class SSHFirewall(app_manager.RyuApp):
         super(SSHFirewall, self).__init__(*args, **kwargs)
         self.ssh_attempts = {}
         self.blocked_ips = set()
+        self.logger.propagate = False  # Disable propagation to root logger
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(stream_handler)
 
     @handler.set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
@@ -29,6 +36,7 @@ class SSHFirewall(app_manager.RyuApp):
                 tcp = pkt.get_protocol(tcp.tcp)
                 if tcp.dst_port == 22:  # SSH port
                     src_ip = ip.src
+                    self.logger.debug(f"Received SSH packet from {src_ip}")
                     now = datetime.now()
 
                     if src_ip not in self.ssh_attempts:
@@ -40,7 +48,7 @@ class SSHFirewall(app_manager.RyuApp):
                     if len(self.ssh_attempts[src_ip]) > 10 and src_ip not in self.blocked_ips:
                         self.install_block_rule(dp, parser, ofp, src_ip)
                         self.blocked_ips.add(src_ip)
-                        self.logger.info("Blocked SSH brute-force attempt from %s", src_ip)
+                        self.logger.info(f"Blocked SSH brute-force attempt from {src_ip}")
 
     def install_block_rule(self, dp, parser, ofp, src_ip):
         match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_src=src_ip)
@@ -48,3 +56,4 @@ class SSHFirewall(app_manager.RyuApp):
         inst = [parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
         mod = parser.OFPFlowMod(datapath=dp, match=match, instructions=inst)
         dp.send_msg(mod)
+        self.logger.debug(f"Installed block rule for {src_ip}")
