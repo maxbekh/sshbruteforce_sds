1f2d4020bd50ac43ba3e497f69bb428ab5a1b5c0
BEKHEDDA Maxence <maxence.bekhedda@telecomnancy.eu>
2024-05-13 20:47:29 +0200

    new attempt for firewall

--- ryu/ryu_firewall.py
+++ ryu/ryu_firewall.py
@@ -1,61 +1,61 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller import handler
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import tcp
 from ryu.ofproto import ofproto_v1_3
-from datetime import datetime, timedelta
-import logging
+import time
 
-class SSHFirewall(app_manager.RyuApp):
+class SSHConnectionMonitor(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        print("SSHFirewall::__init__")
-        super(SSHFirewall, self).__init__(*args, **kwargs)
-        self.ssh_attempts = {}
+        super(SSHConnectionMonitor, self).__init__(*args, **kwargs)
+        self.ssh_connections = {}
         self.blocked_ips = set()
-        self.logger.propagate = False  # Disable propagation to root logger
-        self.logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
-        self.logger.addHandler(stream_handler)
 
-    @handler.set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
-        print("SSHFirewall::packet_in_handler")
         msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        parser = dp.ofproto_parser
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
+        ip4 = pkt.get_protocol(ipv4.ipv4)
+        tcp_pkt = pkt.get_protocol(tcp.tcp)
 
-        if eth.ethertype == ether_types.ETH_TYPE_IP:
-            ip = pkt.get_protocol(ipv4.ipv4)
-            if ip.proto == inet.IPPROTO_TCP:
-                tcp = pkt.get_protocol(tcp.tcp)
-                if tcp.dst_port == 22:  # SSH port
-                    src_ip = ip.src
-                    self.logger.debug(f"Received SSH packet from {src_ip}")
-                    now = datetime.now()
+        if ip4 and tcp_pkt and tcp_pkt.dst_port == 22:
+            src_ip = ip4.src
+            self.log_ssh_attempt(src_ip)
+            if self.check_ssh_threshold(src_ip):
+                self.block_ip(datapath, parser, ofproto, src_ip)
 
-                    if src_ip not in self.ssh_attempts:
-                        self.ssh_attempts[src_ip] = []
+    def log_ssh_attempt(self, src_ip):
+        self.logger.info(f"SSH connection attempt from {src_ip}")
+        if src_ip not in self.ssh_connections:
+            self.ssh_connections[src_ip] = [time.time()]
+        else:
+            self.ssh_connections[src_ip].append(time.time())
 
-                    self.ssh_attempts[src_ip] = [attempt for attempt in self.ssh_attempts[src_ip] if attempt > now - timedelta(minutes=10)]
-                    self.ssh_attempts[src_ip].append(now)
+    def check_ssh_threshold(self, src_ip):
+        if src_ip not in self.blocked_ips:
+            now = time.time()
+            ten_minutes_ago = now - 600
+            connection_times = self.ssh_connections.get(src_ip, [])
+            recent_connections = [t for t in connection_times if t > ten_minutes_ago]
+            if len(recent_connections) > 10:
+                return True
+        return False
 
-                    if len(self.ssh_attempts[src_ip]) > 10 and src_ip not in self.blocked_ips:
-                        self.install_block_rule(dp, parser, ofp, src_ip)
-                        self.blocked_ips.add(src_ip)
-                        self.logger.info(f"Blocked SSH brute-force attempt from {src_ip}")
-
-    def install_block_rule(self, dp, parser, ofp, src_ip):
-        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_src=src_ip)
+    def block_ip(self, datapath, parser, ofproto, src_ip):
+        self.logger.info(f"Blocking IP address {src_ip} due to excessive SSH connection attempts")
+        self.blocked_ips.add(src_ip)
+        match = parser.OFPMatch(eth_type=0x0800, ipv4_src=src_ip)
         actions = []
-        inst = [parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
-        mod = parser.OFPFlowMod(datapath=dp, match=match, instructions=inst)
-        dp.send_msg(mod)
-        self.logger.debug(f"Installed block rule for {src_ip}")
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=1, match=match, instructions=inst)
+        datapath.send_msg(mod)
