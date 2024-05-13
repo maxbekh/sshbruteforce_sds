e2997b70d7927e5e9e1dcc4dc3df58259d9708e6
BEKHEDDA Maxence <maxence.bekhedda@telecomnancy.eu>
2024-05-13 20:50:06 +0200

    firewall fix

--- ryu/ryu_firewall.py
+++ ryu/ryu_firewall.py
@@ -1,6 +1,6 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ipv4
